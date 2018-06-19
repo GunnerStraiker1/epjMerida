fef625a4636b8d67fa8aa6bb575a5376e09ff23c
GunnerStraiker1 <victorcito001@hotmail.com>
2018-06-19 17:34:03 -0500

    Prueba responsividad imagenes 3

--- webNewDesign/Home.php
+++ webNewDesign/Home.php
@@ -59,14 +59,14 @@
           <div class="col-xs-12 col-sm-12 title">
             Próximas Actividades
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-4 actividades">
-            <img src="../img/act6.jpg" alt="" >
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 actividades">
+            <img src="../img/act6.jpg" alt="" class="img-fluid">
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-4 actividades">
-            <img src="../img/act4.jpg" alt="" >
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 actividades">
+            <img src="../img/act4.jpg" alt="" class="img-fluid">
           </div>
-          <div class="col-xs-12 col-sm-12 col-md-4 actividades">
-            <img src="../img/act1.jpg" alt="" >
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 actividades">
+            <img src="../img/act1.jpg" alt="" class="img-fluid">
           </div>
         </div>
       </div>

9399991eb30d606dfa9f4d4d51e1b407f5d12389
Huruz <huruz_wwe@hotmail.com>
2018-06-22 01:05:57 -0500

    Términos

---
+++ webNewDesign/Term_Cond.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/Menu.css">
+    <link rel="stylesheet" href="../css/term_cond.css">
+    <link rel="shortcut icon" href="../img/favicon.ico">
+    <title>Términos y Condiciones</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </head>
+  <?php
+    include "Menu.php";
+  ?>
+  <body>
+    <div class="container">
+        <div class="container-fluid">
+            <h2 class="text-center">Términos y Condiciones del EmproRally 2K18</h2>
+            <p class="text-justify">
+              Al acceder y leer el siguiente documento, usted acepta y accede a estar obligado por los términos y 
+              disposiciones de este acuerdo. Asimismo, al ser parte de este evento, usted estará sujeto a toda regla o 
+              guía de uso correspondiente que se haya publicado para dichos evento. Toda participación en este evento 
+              constituirá la aceptación de este acuerdo. Si no acepta cumplir con lo anterior, por favor, no prosiga 
+              con el registro.
+            </p>
+            <h4 class="text-lg font-weight-bold">Aspecto del uso de automóviles</h4>
+            <p>
+              <ul>
+                <li>Seguro del coche.</li>
+                <li>Licencias de conducir vigentes, mínimo dos.</li>
+                <li>Kit de primeros auxilios.</li>
+                <li>Llanta(s) de refacción en buenas condiciones.</li>
+                <li>Llave de cruz.</li>
+                <li>Extintor lleno.</li>
+                <li>Dispositivos portátiles de señalización de peligro homologados, es decir, triángulos.</li>
+                <li>Conos vehiculares.</li>
+                <li>Dos linternas.</li>
+              </ul>
+            </p>
+            <h4 class="text-lg font-weight-bold">Requisitos previos al registro del equipo</h4>
+            <p>
+              La cuota de inscripción es de $100.00 MXN. Dicho pago es único y no reembolsable, el cual se debe realizar 
+              un depósito a la cuenta <u><strong>4766 8406 8279 5294</strong></u>. </br>
+              <div class="d-inline-flex">
+                <div class="text-right alert alert-warning" role="alert">
+                    <em>La cuenta es parte del servicio <strong>Saldazo</strong> de Oxxo, por lo cual el depósito puede realizarse 
+                    en cualquier establecimiento Oxxo.</em>
+                </div>
+              </div>
+            </p>
+            <p>
+              Los integrantes del equipo deben estar conformados por 4 emproistas y 1 no emproista.
+            </p>
+            <h4 class="text-lg font-weight-bold">Registro</h4>
+            <p>
+              Una vez cerciorada y validada la información dada, continúe con el registro del equipo en esta <a href="registro-rally.php">sección</a>.</br>
+              Ingresar los siguientes datos correctamente: <strong>Nombre del equipo, Nombre completo de los integrantes, Número celular, 
+              Encuentro al que pertenecen y Correo electrónico.</strong> Tomar en cuenta la selección del integrante que va a ser el capitán del equipo, 
+              ya que una vez guardado el registro, se enviará un correo electrónico al capitán seleccionado para confirmar el registro.</br> 
+            </p>
+            <p>
+              Al final de la sección, se deben anexar las fotografías de las dos licencias de conducir y el Boucher del pago de la inscripción.
+            </p>
+            <p>
+              <a href="../Términos y Condiciones.pdf" download="Término-Condiciones-EmproRally" style="color: #FBA05F;">
+                <span style="color: #DD7C38;"><strong>Descargar archivo</strong></span>
+                <img src="../img/dpdf.png" width="5%" height="auto" id="pdf"/>
+              </a>      
+            </p>
+        </div>
+    </div>
+    <div class="container-fluid" id="footer">
+      <div class="row">
+        <div class="col-sm-12 col-md-6" id="contact">
+            <h5>Encuentros de Promoción Juvenil Centro Guias Mérida</h5>
+            <h6>Ubicación: Parroquia de Santiago Apóstol</h6>
+            <h6>Mérida, Yucatán, México</h6>
+          </div>
+          <div class="col-sm-12 col-md-6" id="devContact">
+            <h6></h6>
+            <h6>Webpage created by Aktan Technology</h6>
+            <h6>Mérida, Yucatán, México</h6>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>

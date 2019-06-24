fdfeb6f05906d5a366b9b0bc588cf88a6aa5de3e
GunnerStraiker1 <victorcito001@hotmail.com>
2019-06-23 21:15:11 -0500

    DB Changes

--- webNewDesign/registro-rally.php
+++ webNewDesign/registro-rally.php
@@ -166,14 +166,16 @@
 </html>
 
 <?php
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\Exception;
+  // use PHPMailer\PHPMailer\PHPMailer;
+  // use PHPMailer\PHPMailer\Exception;
   //C:\xampp\htdocs\vendor\phpmailer\phpmailer\src
-  require '../vendor/phpmailer/phpmailer/src/Exception.php';
-  require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-  require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+  // require '../vendor/phpmailer/phpmailer/src/Exception.php';
+  // require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+  // require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
   require '../vendor/autoload.php';
+  // require('../vendor/sendgrid/sendgrid/sendgrid-php.php')
+  
 
   if (isset($_POST["terminos"])) {
     $database = getenv('DATABASE_URL');
@@ -204,46 +206,46 @@
         $row = pg_fetch_row($rs);
       }
 
-      $mail = new PHPMailer(true);
+      // $mail = new PHPMailer(true);
       try{
-        $mail->isSMTP();
-        $mail->Host = 'smtp.sendgrid.net'; //'smtp.office365.com'; //Servidor smtp del correo
-        $mail->SMTPAuth = true;
-        $mail->Username = getenv('app100048941@heroku.com'); //Correo electronico del remitente
-        $mail->Password = getenv('GunnerStraiker1'); //Constraseña del remitente
+        // $mail->isSMTP();
+        // $mail->Host = 'smtp.sendgrid.net'; //'smtp.office365.com'; //Servidor smtp del correo
+        // $mail->SMTPAuth = true;
+        // $mail->Username = getenv('SENDGRID_USERNAME'); //Correo electronico del remitente
+        // $mail->Password = getenv('GunnerStraiker1'); //Constraseña del remitente
 
-        $mail->SMTPSecure = 'tls'; //Tipo de seguridad
-        $mail->Port = 587; //Puerto del servidor smtp
+        // $mail->SMTPSecure = 'tls'; //Tipo de seguridad
+        // $mail->Port = 587; //Puerto del servidor smtp
 
         //-------------------------------------------------------
 
         // Datos del Correo
 
-        $mail->SetFrom(getenv('SENDGRID_USERNAME'), utf8_decode("Servidor EPJ")); //Correo electronico del remitente y nombre(debe coindidir con el username)
-        $mail->AddAddress('pinelojuancarlos@gmail.com', utf8_decode("Juan Carlos Pinelo")); //Correo electronico  y nombre del destinatario
-        $mail->AddBCC('victorox100@gmail.com', utf8_decode("Victor Perera")); //Correo electronico  y nombre del destinatario
-        // $mail->AddAddress('endimion_07@hotmail.com', utf8_decode("Javier Balam")); //Correo electronico  y nombre del destinatario
-        $mail->addReplyTo(utf8_decode($user1[2]), utf8_decode($user1[0]));
+        // $mail->SetFrom(getenv('SENDGRID_USERNAME'), utf8_decode("Servidor EPJ")); //Correo electronico del remitente y nombre(debe coindidir con el username)
+        // $mail->AddAddress('pinelojuancarlos@gmail.com', utf8_decode("Juan Carlos Pinelo")); //Correo electronico  y nombre del destinatario
+        // $mail->AddBCC('victorox100@gmail.com', utf8_decode("Victor Perera")); //Correo electronico  y nombre del destinatario
+        // // $mail->AddAddress('endimion_07@hotmail.com', utf8_decode("Javier Balam")); //Correo electronico  y nombre del destinatario
+        // $mail->addReplyTo(utf8_decode($user1[2]), utf8_decode($user1[0]));
 
-        $mail->isHTML(true);
-        $mail->Subject = utf8_decode(utf8_decode("Registro de Equipo EMPRORALLY 2018")); //Asunto del correo electronico
-        $mail->Body = utf8_decode(
-          'Datos del Equipo: <br>'
-          .'<b>Nombre del Equipo: '.utf8_decode($teamName).'</b><br>'
-          .'<b>Capitán: </b>'.utf8_decode($user1[0]).', Telefono: '.utf8_decode($user1[1]).', EPJ: '.utf8_decode($user1[3]).'<br>'
-          .'Integrante 2: '.utf8_decode($user2[0]).', Telefono: '.utf8_decode($user2[1]).', EPJ: '.utf8_decode($user2[3]).'<br>'
-          .'Integrante 3: '.utf8_decode($user3[0]).', Telefono: '.utf8_decode($user3[1]).', EPJ: '.utf8_decode($user3[3]).'<br>'
-          .'Integrante 4: '.utf8_decode($user4[0]).', Telefono: '.utf8_decode($user4[1]).', EPJ: '.utf8_decode($user4[3]).'<br>'
-          .'Integrante 5: '.utf8_decode($user5[0]).', Telefono: '.utf8_decode($user5[1]).'<br>'
-          ."<b><i>PROPUESTA DE TEMATICA DE EQUIPO: ".$row[0]."</i></b> <br><br>"
-          .'<i>Hola!, soy el servidor EPJ, te recuerdo enviar un mensaje de Confirmación al Capitán del equipo mencionando su temática</i>');
+        // $mail->isHTML(true);
+        // $mail->Subject = utf8_decode(utf8_decode("Registro de Equipo EMPRORALLY 2018")); //Asunto del correo electronico
+        // $mail->Body = utf8_decode(
+        //   'Datos del Equipo: <br>'
+        //   .'<b>Nombre del Equipo: '.utf8_decode($teamName).'</b><br>'
+        //   .'<b>Capitán: </b>'.utf8_decode($user1[0]).', Telefono: '.utf8_decode($user1[1]).', EPJ: '.utf8_decode($user1[3]).'<br>'
+        //   .'Integrante 2: '.utf8_decode($user2[0]).', Telefono: '.utf8_decode($user2[1]).', EPJ: '.utf8_decode($user2[3]).'<br>'
+        //   .'Integrante 3: '.utf8_decode($user3[0]).', Telefono: '.utf8_decode($user3[1]).', EPJ: '.utf8_decode($user3[3]).'<br>'
+        //   .'Integrante 4: '.utf8_decode($user4[0]).', Telefono: '.utf8_decode($user4[1]).', EPJ: '.utf8_decode($user4[3]).'<br>'
+        //   .'Integrante 5: '.utf8_decode($user5[0]).', Telefono: '.utf8_decode($user5[1]).'<br>'
+        //   ."<b><i>PROPUESTA DE TEMATICA DE EQUIPO: ".$row[0]."</i></b> <br><br>"
+        //   .'<i>Hola!, soy el servidor EPJ, te recuerdo enviar un mensaje de Confirmación al Capitán del equipo mencionando su temática</i>');
 
-          foreach ($_FILES["archivos"]["name"] as $k => $v) {
-            $mail->AddAttachment( $_FILES["archivos"]["tmp_name"][$k],'Uploaded file.jpg' );
-          }
+        //   foreach ($_FILES["archivos"]["name"] as $k => $v) {
+        //     $mail->AddAttachment( $_FILES["archivos"]["tmp_name"][$k],'Uploaded file.jpg' );
+        //   }
         //Envio del mail
 
-        $mail->send();
+        // $mail->send();
 
         //Insertar Equipo en DB
         $sqlCreateTeam = "INSERT INTO teams (teamname, tematic) VALUES ('".$teamName."','".$row[0]."')";
